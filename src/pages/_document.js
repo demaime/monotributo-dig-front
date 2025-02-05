e13d488a0a1bdb6c4a12edb2480cc28f4dd3edb2
Demian <aimedemian@gmail.com>
2025-02-05 11:13:10 -0300

    agregado script analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-PWB1KMS0WG`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PWB1KMS0WG');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
